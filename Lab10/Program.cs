6ab5c97788659fba4630a9b40beb5a745021149b
Mozzey <mbcacciano@gmail.com>
2018-07-26 13:53:23 -0400

    working on display methods

---
+++ Lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Lab10ClassLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+            Console.Write("Enter Raidus: ");
+            double radius = double.Parse(Console.ReadLine());
+            var circle = new Circle(radius);
+
+            Console.WriteLine(circle); // for testing erase later
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab10ClassLibrary/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab10ClassLibrary
+{
+    public class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+            Pi = Math.PI;
+        }
+        
+        public double CalculateCircumference()
+        {
+            return 2 * Pi * Radius;
+        }
+        
+        /*
+        public string CalculateFormattedCircumference()
+        {
+
+        }
+        */
+        public double CalculateArea()
+        {
+          return Pi * (Radius * Radius);  
+        }
+        /*
+        public string CalculateFormattedArea()
+        {
+
+        }
+        */
+        private string FormatNumber(double x)
+        {
+            return $"{x:0.00}";
+        }
+        private double Radius { get; set; }
+        public double Pi { get; private set; }
+    }
+}
